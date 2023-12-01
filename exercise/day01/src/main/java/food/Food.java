ba41ef0a815172b0897065fbdf23fb64c79e7689
Johan Beaucé <jbeauce@pictet-technologies.com>
2023-12-01 23:53:54 +0100

    Simplify the reading of editable condition

--- exercise/day01/src/main/java/food/Food.java
+++ exercise/day01/src/main/java/food/Food.java
@@ -4,12 +4,23 @@ import java.time.LocalDate;
 import java.util.UUID;
 import java.util.function.Supplier;
 
-public record Food(LocalDate expirationDate,
-                   Boolean approvedForConsumption,
-                   UUID inspectorId) {
-    public boolean isEdible(Supplier<LocalDate> now) {
-        return this.expirationDate.isAfter(now.get()) &&
-                this.approvedForConsumption &&
-                this.inspectorId != null;
+public record Food(
+        LocalDate expirationDate,
+        Boolean approvedForConsumption,
+        UUID inspectorId
+) {
+    public boolean isEdible(Supplier<LocalDate> dateSupplier) {
+        var givenDate = dateSupplier.get();
+        return isExpired(givenDate) &&
+                approvedForConsumption &&
+                inspectorIdIsProvided();
+    }
+
+    private boolean isExpired(LocalDate date) {
+        return expirationDate.isAfter(date);
+    }
+
+    private boolean inspectorIdIsProvided() {
+        return inspectorId != null;
     }
 }
