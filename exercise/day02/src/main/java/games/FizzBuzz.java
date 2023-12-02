cd64ccb85b2cfd0c51c2c40ef904e1b9ae6edf23
Johan Beaucé <jbeauce@pictet-technologies.com>
2023-12-02 22:41:01 +0100

    Use early return and well named methods

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -3,25 +3,39 @@ package games;
 public class FizzBuzz {
     private FizzBuzz() {
     }
-    
+
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+        // use early return to avoid nested if statements
+        if (isOutOfRange(input)) {
             throw new OutOfRangeException();
         }
+        // use well named methods to make the code more readable
+        if (isDivisibleByThreeAndFive(input)) {
+            return "FizzBuzz";
+        }
+        if (isDivisibleByThree(input)) {
+            return "Fizz";
+        }
+        if (isDivisibleByFive(input)) {
+            return "Buzz";
+        }
+
+        return input.toString();
+    }
+
+    private static boolean isOutOfRange(Integer input) {
+        return input <= 0 || input > 100;
+    }
+
+    private static boolean isDivisibleByThree(Integer input) {
+        return input % 3 == 0;
+    }
+
+    private static boolean isDivisibleByThreeAndFive(Integer input) {
+        return input % 3 == 0 && input % 5 == 0;
+    }
+
+    private static boolean isDivisibleByFive(Integer input) {
+        return input % 5 == 0;
     }
 }
