3dbdfbe01c8fea3e3c79bd9e93f3458853242cdc
Johan Beaucé <jbeauce@pictet-technologies.com>
2023-12-03 21:51:38 +0100

    Use static import for enums put stream on multi lines extract comparator to increase readability

--- exercise/day03/src/test/java/PopulationTests.java
+++ exercise/day03/src/test/java/PopulationTests.java
@@ -2,19 +2,18 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import people.Person;
 import people.Pet;
-import people.PetType;
 
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
-import static java.lang.Integer.MAX_VALUE;
-import static java.lang.String.format;
-import static java.lang.System.lineSeparator;
 import static org.assertj.core.api.Assertions.assertThat;
+import static people.PetType.BIRD;
+import static people.PetType.CAT;
+import static people.PetType.DOG;
+import static people.PetType.HAMSTER;
+import static people.PetType.SNAKE;
+import static people.PetType.TURTLE;
 
 class PopulationTests {
     private static List<Person> population;
@@ -22,31 +21,44 @@ class PopulationTests {
     @BeforeAll
     static void setup() {
         population = Arrays.asList(
+                // use static import for enum to be less verbose
                 new Person("Peter", "Griffin")
-                        .addPet(PetType.CAT, "Tabby", 2),
+                        .addPet(CAT, "Tabby", 2),
                 new Person("Stewie", "Griffin")
-                        .addPet(PetType.CAT, "Dolly", 3)
-                        .addPet(PetType.DOG, "Brian", 9),
+                        .addPet(CAT, "Dolly", 3)
+                        .addPet(DOG, "Brian", 9),
                 new Person("Joe", "Swanson")
-                        .addPet(PetType.DOG, "Spike", 4),
+                        .addPet(DOG, "Spike", 4),
                 new Person("Lois", "Griffin")
-                        .addPet(PetType.SNAKE, "Serpy", 1),
+                        .addPet(SNAKE, "Serpy", 1),
                 new Person("Meg", "Griffin")
-                        .addPet(PetType.BIRD, "Tweety", 1),
+                        .addPet(BIRD, "Tweety", 1),
                 new Person("Chris", "Griffin")
-                        .addPet(PetType.TURTLE, "Speedy", 4),
+                        .addPet(TURTLE, "Speedy", 4),
                 new Person("Cleveland", "Brown")
-                        .addPet(PetType.HAMSTER, "Fuzzy", 1)
-                        .addPet(PetType.HAMSTER, "Wuzzy", 2),
+                        .addPet(HAMSTER, "Fuzzy", 1)
+                        .addPet(HAMSTER, "Wuzzy", 2),
                 new Person("Glenn", "Quagmire")
         );
     }
 
     @Test
     void whoOwnsTheYoungestPet() {
-        var filtered = population.stream().min(Comparator.comparingInt(person -> person.pets().stream().mapToInt(Pet::age).min().orElse(Integer.MAX_VALUE))).orElse(null);
+        // always go to next line in a stream, it is easier to read
+        var filtered = population.stream()
+                // extracting comparator in a method facilitating readability, I want to concentrate on my business logic
+                // I let this method in the test because I don't want to add production code for my test purpose
+                .min(Comparator.comparingInt(this::getYoungestPetAge))
+                .orElse(null);
 
         assert filtered != null;
         assertThat(filtered.firstName()).isEqualTo("Lois");
+    }
+
+    private int getYoungestPetAge(Person person) {
+        return person.pets().stream()
+                .mapToInt(Pet::age)
+                .min()
+                .orElse(Integer.MAX_VALUE);
     }
 }
