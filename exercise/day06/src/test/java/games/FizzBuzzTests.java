52b5f427b765c1de5db80880e11dd75d15f77438
Johan Beaucé <jbeauce@pictet-technologies.com>
2023-12-09 12:51:11 +0100

    Day-6 - Use method source for parameterized test

--- exercise/day06/src/test/java/games/FizzBuzzTests.java
+++ exercise/day06/src/test/java/games/FizzBuzzTests.java
@@ -1,39 +1,39 @@
 package games;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FizzBuzzTests {
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 67, 82})
-    void returns_the_given_number_for(int input) throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(input))
-                .isEqualTo(String.valueOf(input));
+    public static Stream<Arguments> getInputs() {
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(67, "67"),
+                Arguments.of(82, "82"),
+                Arguments.of(3, "Fizz"),
+                Arguments.of(66, "Fizz"),
+                Arguments.of(99, "Fizz"),
+                Arguments.of(5, "Buzz"),
+                Arguments.of(50, "Buzz"),
+                Arguments.of(85, "Buzz"),
+                Arguments.of(15, "FizzBuzz"),
+                Arguments.of(30, "FizzBuzz"),
+                Arguments.of(45, "FizzBuzz")
+        );
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 66, 99})
-    void returns_Fizz_for(int input) throws OutOfRangeException {
+    @MethodSource("getInputs")
+    void returns_the_given_number_for(int input, String result) throws OutOfRangeException {
         assertThat(FizzBuzz.convert(input))
-                .isEqualTo("Fizz");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {5, 50, 85})
-    void returns_Buzz_for(int input) throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(input))
-                .isEqualTo("Buzz");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {15, 30, 45})
-    void returns_FizzBuzz_for(int input) throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(input))
-                .isEqualTo("FizzBuzz");
+                .isEqualTo(result);
     }
 
     @ParameterizedTest
